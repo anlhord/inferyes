4fb280877500150117502651483e4395bf5def2b
anlhord <anlhord@gmail.com>
2014-12-21 21:25:30 +0100

    Reduce the useless options

--- main.go
+++ main.go
@@ -14,8 +14,25 @@ func missingAction(c *cli.Context) {
 	fmt.Fprintln(os.Stderr, "TODO :)")
 }
 
+type Fooer struct{}
+func (s *Fooer) Visit(node ast.Node) ast.Visitor {
+	print("Oh such a beautiful node\n")
+	return s
+}
+
 func mainAction(c *cli.Context) {
-	fmt.Fprintln(os.Stderr, "TODO :)")
+
+	codefile := c.GlobalString("f")
+
+	fsetc := token.NewFileSet()
+	fc, _ := parser.ParseFile(fsetc, codefile, nil, 0)
+
+	fooer := Fooer{}
+
+	for _, s := range fc.Decls {
+		ast.Walk(&fooer, s)
+	}
+
 }
 
 func main() {
@@ -24,11 +41,7 @@ func main() {
 	flagz := []cli.Flag{
 		cli.StringFlag{
 			Name:  "f",
-			Usage: "Foo",
-		},
-		cli.StringFlag{
-			Name:  "b",
-			Usage: "Bar",
+			Usage: "File",
 		},
 	}
 
@@ -45,100 +58,12 @@ func main() {
 			Usage:  "Inferyes inference engine",
 			Action: mainAction,
 		},
-		{
-			Name: "separate",
-			Flags: []cli.Flag{
-				cli.BoolFlag{
-					Name:  "f",
-					Usage: "Foo.",
-				},
-			},
-			Usage:  "Separate splits and strips errors handling",
-			Action: missingAction,
-		},
-		{
-			Name: "merge",
-			Flags: []cli.Flag{
-				cli.BoolFlag{
-					Name:  "f",
-					Usage: "Foo.",
-				},
-			},
-			Usage:  "Merge handlers back",
-			Action: missingAction,
-		},
-		{
-			Name: "split",
-			Flags: []cli.Flag{
-				cli.BoolFlag{
-					Name:  "f",
-					Usage: "Foo.",
-				},
-			},
-			Usage:  "Split off the current error handling to an errfile",
-			Action: missingAction,
-		},
-		{
-			Name: "strip",
-			Flags: []cli.Flag{
-				cli.BoolFlag{
-					Name:  "f",
-					Usage: "Foo.",
-				},
-			},
-			Usage:  "Strip off the error handling from the client code",
-			Action: missingAction,
-		},
-		{
-			Name: "init",
-			Flags: []cli.Flag{
-				cli.BoolFlag{
-					Name:  "f",
-					Usage: "Foo.",
-				},
-			},
-			Usage:  "Create an empty errfile",
-			Action: missingAction,
-		},
-		{
-			Name: "ls",
-			Flags: []cli.Flag{
-				cli.BoolFlag{
-					Name:  "u",
-					Usage: "Unhandled errors only.",
-				},
-			},
-			Usage:  "List the handling of all errors",
-			Action: missingAction,
-		},
-		{
-			Name: "show",
-			Flags: []cli.Flag{
-				cli.BoolFlag{
-					Name:  "f",
-					Usage: "Foo.",
-				},
-			},
-			Usage:  "Show handling related to a function",
-			Action: missingAction,
-		},
-		{
-			Name: "status",
-			Flags: []cli.Flag{
-				cli.BoolFlag{
-					Name:  "f",
-					Usage: "Foo.",
-				},
-			},
-			Usage:  "Show cumulative statistics",
-			Action: missingAction,
-		},
 	}
 
 	app := cli.NewApp()
 	app.Flags = flagz
 	app.Commands = cmdz
-	app.Usage = "strip / add error handling to a go file"
+	app.Usage = "infer types for autotype slices."
 	app.Version = "0.0.0"
 	app.Action = missingAction
 
